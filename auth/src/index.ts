7a1c29863d65c49c3def7571343fcd6d5c7e155d
Hisham Paloli <hishampaloli786@gmail.com>
2022-12-01 20:44:07 +0530

    mongoose

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { json } from "body-parser";
 import colors from "colors";
 import "express-async-errors";
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
@@ -23,6 +25,18 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log(`786 - AUTH SERVER AT 3000`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("DATABSAE CONNECTED");
+    
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log(`786 - AUTH SERVER AT 3000`);
+  });
+};
+
+start();
