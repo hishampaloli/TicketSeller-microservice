33a63a160bbb342e37932825c75d891d5190e7d4
Hisham Paloli <hishampaloli786@gmail.com>
2022-12-02 16:05:49 +0530

    auth

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,15 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
+router.get(
+  "/api/users/currentuser",
+  currentUser,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
   }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (error) {
-    res.send({ currentUser: null });
-  }
-});
+);
 
 export { router as currentUserRouter };
